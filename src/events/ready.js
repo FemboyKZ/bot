1d86de92b40bcb80e9f35aa3f2a429d622fae08c
Dots <dotvnl@gmail.com>
2024-04-10 01:56:05 +0300

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const mongodbURL = process.env.MONGODBURL;
 
 mongoose.set("strictQuery", false);
-// mongoose.set('keepAlive', true); deprecated
 
 module.exports = {
   name: "ready",
@@ -15,29 +14,11 @@ module.exports = {
     await mongoose.createConnection(mongodbURL || ``, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
+      // keepAlive: true, deprecated
     });
 
     if (mongoose.createConnection) {
       console.log("The MongoDB Database is running.");
-    }
-
-    async function pickPresence() {
-      const option = Math.floor(Math.random() * statusArray.length);
-
-      try {
-        await client.user.setPresence({
-          activities: [
-            {
-              name: statusArray[option].content,
-              type: statusArray[option].type,
-            },
-          ],
-
-          status: statusArray[option].status,
-        });
-      } catch (error) {
-        console.error(error);
-      }
     }
   },
 };
