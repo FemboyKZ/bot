e6164c7d1daf945fb45caacb79788a19f1220ed7
Dots <dotvnl@gmail.com>
2024-04-10 17:17:53 +0300

    send log fix?

--- src/index.js
+++ src/index.js
@@ -1739,17 +1739,28 @@ client.on(Events.GuildMemberUpdate, async (oldMember, newMember) => {
   if (oldMember.roles.cache.size > newMember.roles.cache.size) {
     oldMember.roles.cache.forEach((role) => {
       if (!newMember.roles.cache.has(role.id)) {
-        auditEmbed.addFields({
-          name: "Role Removed: ",
-          value: `${role}`,
-        });
+        auditEmbed.addFields(
+          {
+            name: "Role Removed: ",
+            value: `${role}`,
+            inline: false,
+          },
+          { name: "ID:", value: newMember.id, inline: false }
+        );
+        auditChannel.send({ embeds: [auditEmbed] });
+        return;
       }
     });
   }
   if (oldMember.roles.cache.size < newMember.roles.cache.size) {
     newMember.roles.cache.forEach((role) => {
       if (!oldMember.roles.cache.has(role.id)) {
-        auditEmbed.addFields({ name: "Role Added: ", value: `${role}` });
+        auditEmbed.addFields(
+          { name: "Role Added: ", value: `${role}`, inline: false },
+          { name: "ID:", value: newMember.id, inline: false }
+        );
+        auditChannel.send({ embeds: [auditEmbed] });
+        return;
       }
     });
   }
