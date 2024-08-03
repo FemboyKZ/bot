8f5fa2671e1e089c3561f9f22f2e46f4227333d4
Dots <dotvnl@gmail.com>
2024-08-03 04:24:35 +0300

    fix whitelist responses

--- src/whitelist-response.js
+++ src/whitelist-response.js
@@ -1,4 +1,4 @@
-const { PermissionsBitField, Events } = require("discord.js");
+const { PermissionFlagsBits, Events } = require("discord.js");
 const whitelistStatusSchema = require("./Schemas/whitelistStatusSchema");
 const { client } = require("./index.js");
 
@@ -7,32 +7,35 @@ client.on(Events.MessageReactionAdd, async (reaction, user) => {
   if (user.partial) await user.fetch();
   if (reaction.message.partial) await reaction.message.fetch();
   if (reaction.partial) await reaction.fetch();
-  if (!user.PermissionsBitFields.has(PermissionsBitField.Flags.Administrator))
-    return;
-  const data = await whitelistStatusSchema.findOne({
-    Request: user.id,
-    Status: null,
-  });
-  if (!data) return;
-  const guild = await client.guilds.fetch(data.Guild);
-  const member = await guild.members.fetch(user.id);
-  const role = guild.roles.cache.find((role) => role.name === "Femmy");
-  const oldRole = member.roles.cache.find(
-    (role) => role.name === "Wannabe Fem"
-  );
+  const member = reaction.message.guild.members.cache.get(user.id);
+  if (!member.permissions.has(PermissionFlagsBits.Administrator)) return;
+
   try {
+    const data = await whitelistStatusSchema.findOne({
+      Request: user.id,
+    });
+    if (!data) return;
+    const guild = reaction.message.guild;
+    const member = guild.members.cache.get(user.id);
+    if (!member || !guild) return;
+    const role = guild.roles.cache.find((role) => role.name === "Femmy");
+    const oldRole = member.roles.cache.find(
+      (role) => role.name === "Wannabe Fem"
+    );
     if (reaction.emoji.name === "👍") {
       await whitelistStatusSchema.findOneAndUpdate(
-        { Request: user.id, Status: null },
+        { Request: user.id },
         { Status: true }
       );
-      await member.roles.add(role);
-      await member.roles.remove(oldRole);
+      if (!role) await member.roles.add(role);
+      if (oldRole) await member.roles.remove(oldRole);
     } else if (reaction.emoji.name === "👎") {
       await whitelistStatusSchema.findOneAndUpdate(
-        { Request: user.id, Status: null },
+        { Request: user.id },
         { Status: false }
       );
+    } else {
+      return;
     }
   } catch (error) {
     console.log(error);
