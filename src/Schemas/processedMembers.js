2adbdb29e48bc58dd6cd635386cdb658013b0bd3
Dots <dotvnl@gmail.com>
2024-08-04 20:21:38 +0300

    delete obsolete schema

--- src/Schemas/processedMembers.js
+++
@@ -1,8 +0,0 @@
-const { model, Schema } = require("mongoose");
-
-let processedMembers = new Schema({
-  Guild: String,
-  Channel: String,
-});
-
-module.exports = model("processedMembers", processedMembers);
