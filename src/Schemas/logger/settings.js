00f987705bf746d9563911f152a46ba26421b9a0
Dots <dotvnl@gmail.com>
2024-08-11 20:08:18 +0300

    create schemas for log settings, guild info logs

---
+++ src/Schemas/logger/settings.js
@@ -0,0 +1,21 @@
+const { model, Schema } = require("mongoose");
+
+let settings = new Schema({
+  Guild: String,
+  Post: Boolean, // post in channel?
+  Store: Boolean, // store in db?
+  Automod: Boolean,
+  Bans: Boolean,
+  Channels: Boolean,
+  Emojis: Boolean,
+  Invites: Boolean,
+  Interactions: Boolean,
+  Members: Boolean,
+  Users: Boolean,
+  Messages: Boolean,
+  Roles: Boolean,
+  Stickers: Boolean,
+  Threads: Boolean,
+});
+
+module.exports = model("settings", settings);
