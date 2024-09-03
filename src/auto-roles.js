f7e09a0399770d2b0362e4ab63a18ee16e481e3f
Dots <dotvnl@gmail.com>
2024-09-03 22:34:30 +0300

    apply mute role on join if muted

--- src/auto-roles.js
+++ src/auto-roles.js
@@ -1,5 +1,7 @@
 const { Events } = require("discord.js");
 const schema = require("./Schemas/autorole.js");
+const mutes = require("./Schemas/moderation/mute.js");
+const muteRole = require("./Schemas/moderation/mute-role.js");
 const { client } = require("./index.js");
 
 client.on(Events.GuildMemberAdd, async (member) => {
@@ -12,6 +14,26 @@ client.on(Events.GuildMemberAdd, async (member) => {
       if (role) {
         await member.roles.add(role);
       }
+    }
+  } catch (e) {
+    return console.log(e);
+  }
+});
+
+client.on(Events.GuildMemberAdd, async (member) => {
+  const data = await mutes.findOne({
+    Guild: member.guild.id,
+    User: member.user.id,
+  });
+  const role = await muteRole.findOne({
+    Guild: member.guild.id,
+  });
+  if (!data || !role) return;
+
+  try {
+    const roles = member.guild.roles.cache.get(role.Role);
+    if (roles) {
+      await member.roles.add(roles);
     }
   } catch (e) {
     return console.log(e);
