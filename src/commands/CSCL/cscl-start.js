c783c3e7a359b29c20988d9561d25b1d18211b3d
Dots <dotvnl@gmail.com>
2024-10-20 19:50:54 +0300

    refactor cscl commands to support fkz api, add na servers to command

--- src/commands/CSCL/cscl-start.js
+++ src/commands/CSCL/cscl-start.js
@@ -4,11 +4,14 @@ const {
   EmbedBuilder,
 } = require("discord.js");
 const { exec } = require("child_process");
+const axios = require("axios");
 const wait = require("timers/promises").setTimeout;
 require("dotenv").config();
 
-const username = process.env.DATHOST_USERNAME;
-const password = process.env.DATHOST_PASSWORD;
+const key = process.env.API_KEY;
+const port = process.env.PORT || 8080;
+const apiUrl = new URL(process.env.API_URL);
+const url = `${apiUrl.origin}:${port}${apiUrl.pathname}`;
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -22,9 +25,11 @@ module.exports = {
         .setDescription("Which server do you want to start")
         .setRequired(true)
         .addChoices(
-          { name: "CS:CL FKZ 1 - VNL KZ 128t", value: "cscl-fkz-1" },
-          { name: "CS:CL FKZ 2 - VNL KZ 64t", value: "cscl-fkz-2" },
-          { name: "CS:CL FKZ 3 - KZTimer 128t", value: "cscl-fkz-3" }
+          { name: "CS:CL EU FKZ 1 - VNL KZ 128t", value: "cscl-fkz-1" },
+          { name: "CS:CL EU FKZ 2 - VNL KZ 64t", value: "cscl-fkz-2" },
+          { name: "CS:CL EU FKZ 3 - KZTimer 128t", value: "cscl-fkz-3" },
+          { name: "CS:CL NA FKZ 1 - VNL KZ 128t", value: "cscl-fkz-4" },
+          { name: "CS:CL NA FKZ 2 - VNL KZ 64t", value: "cscl-fkz-5" }
         )
     ),
 
@@ -54,6 +59,16 @@ module.exports = {
         user: "cscl-fkz-3",
         id: null,
       },
+      "cscl-fkz-4": {
+        name: "CS:CL NA - FKZ 1 - VNL KZ 128t",
+        user: "cscl-fkz-1",
+        id: 1,
+      },
+      "cscl-fkz-5": {
+        name: "CS:CL NA - FKZ 2 - VNL KZ 64t",
+        user: "cscl-fkz-2",
+        id: 2,
+      },
     }[servers];
 
     if (!server) {
@@ -65,6 +80,7 @@ module.exports = {
     }
 
     const { name, user, id } = server;
+    const command = `sudo -iu ${user} /home/${user}/csgoserver start`;
 
     if (
       !interaction.member.permissions.has(PermissionFlagsBits.Administrator) &&
@@ -83,20 +99,51 @@ module.exports = {
         embeds: [embed],
         ephemeral: true,
       });
-      exec(
-        `sudo -iu ${user} /home/${user}/csgoserver start`,
-        async (error, stdout, stderr) => {
+      if (id === null) {
+        exec(command, async (error, stdout, stderr) => {
           if (error) console.log(error);
-          //if (stderr) console.log(stderr);
-          //if (stdout) console.log(stdout);
+        });
+        await wait(3000);
+        embed.setDescription(`Started: ${name}`);
+        return await interaction.editReply({
+          embeds: [embed],
+          ephemeral: true,
+        });
+      } else {
+        if (!url || !key) {
+          embed.setDescription("API url and/or key missing.");
+          return await interaction.editReply({
+            embeds: [embed],
+            ephemeral: true,
+          });
         }
-      );
-      await wait(3000);
-      embed.setDescription(`Started: ${name}`);
-      return await interaction.editReply({
-        embeds: [embed],
-        ephemeral: true,
-      });
+        const response = await axios.post(
+          url,
+          {
+            command: command,
+          },
+          {
+            headers: {
+              authorization: `Bearer ${key}`,
+            },
+          }
+        );
+        await wait(3000);
+        if (response.data.status === 200) {
+          embed.setDescription(`Started: ${name}`);
+          return await interaction.editReply({
+            embeds: [embed],
+            ephemeral: true,
+          });
+        } else {
+          console.log(response.status, response.data);
+          embed.setDescription(`Something went wrong while starting: ${name}`);
+          return await interaction.editReply({
+            embeds: [embed],
+            ephemeral: true,
+          });
+        }
+      }
     } catch (error) {
       await interaction.reply({
         content: `Error: ${error}`,
--- src/commands/CSCL/cscl-stop.js
+++ src/commands/CSCL/cscl-stop.js
@@ -4,11 +4,14 @@ const {
   EmbedBuilder,
 } = require("discord.js");
 const { exec } = require("child_process");
+const axios = require("axios");
 const wait = require("timers/promises").setTimeout;
 require("dotenv").config();
 
-const username = process.env.DATHOST_USERNAME;
-const password = process.env.DATHOST_PASSWORD;
+const key = process.env.API_KEY;
+const port = process.env.PORT || 8080;
+const apiUrl = new URL(process.env.API_URL);
+const url = `${apiUrl.origin}:${port}${apiUrl.pathname}`;
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -22,9 +25,11 @@ module.exports = {
         .setDescription("Which server do you want to stop")
         .setRequired(true)
         .addChoices(
-          { name: "CS:CL FKZ 1 - VNL KZ 128t", value: "cscl-fkz-1" },
-          { name: "CS:CL FKZ 2 - VNL KZ 64t", value: "cscl-fkz-2" },
-          { name: "CS:CL FKZ 3 - KZTimer 128t", value: "cscl-fkz-3" }
+          { name: "CS:CL EU FKZ 1 - VNL KZ 128t", value: "cscl-fkz-1" },
+          { name: "CS:CL EU FKZ 2 - VNL KZ 64t", value: "cscl-fkz-2" },
+          { name: "CS:CL EU FKZ 3 - KZTimer 128t", value: "cscl-fkz-3" },
+          { name: "CS:CL NA FKZ 1 - VNL KZ 128t", value: "cscl-fkz-4" },
+          { name: "CS:CL NA FKZ 2 - VNL KZ 64t", value: "cscl-fkz-5" }
         )
     ),
 
@@ -54,6 +59,16 @@ module.exports = {
         user: "cscl-fkz-3",
         id: null,
       },
+      "cscl-fkz-4": {
+        name: "CS:CL NA - FKZ 1 - VNL KZ 128t",
+        user: "cscl-fkz-1",
+        id: 1,
+      },
+      "cscl-fkz-5": {
+        name: "CS:CL NA - FKZ 2 - VNL KZ 64t",
+        user: "cscl-fkz-2",
+        id: 2,
+      },
     }[servers];
 
     if (!server) {
@@ -65,6 +80,7 @@ module.exports = {
     }
 
     const { name, user, id } = server;
+    const command = `sudo -iu ${user} /home/${user}/csgoserver stop`;
 
     if (
       !interaction.member.permissions.has(PermissionFlagsBits.Administrator) &&
@@ -78,25 +94,56 @@ module.exports = {
     }
 
     try {
-      embed.setDescription(`Stopping: ${name}`);
-      await interaction.reply({
-        embeds: [embed],
-        ephemeral: true,
-      });
-      exec(
-        `sudo -iu ${user} /home/${user}/csgoserver stop`,
-        async (error, stdout, stderr) => {
+      if (id === null) {
+        embed.setDescription(`Stopping: ${name}`);
+        await interaction.reply({
+          embeds: [embed],
+          ephemeral: true,
+        });
+        exec(command, async (error, stdout, stderr) => {
           if (error) console.log(error);
-          //if (stderr) console.log(stderr);
-          //if (stdout) console.log(stdout);
+        });
+        await wait(3000);
+        embed.setDescription(`Stopped: ${name}`);
+        return await interaction.editReply({
+          embeds: [embed],
+          ephemeral: true,
+        });
+      } else {
+        if (!url || !key) {
+          embed.setDescription("API url and/or key missing.");
+          return await interaction.editReply({
+            embeds: [embed],
+            ephemeral: true,
+          });
         }
-      );
-      await wait(3000);
-      embed.setDescription(`Stopped: ${name}`);
-      return await interaction.editReply({
-        embeds: [embed],
-        ephemeral: true,
-      });
+        const response = await axios.post(
+          url,
+          {
+            command: command,
+          },
+          {
+            headers: {
+              authorization: `Bearer ${key}`,
+            },
+          }
+        );
+        await wait(3000);
+        if (response.data.status === 200) {
+          embed.setDescription(`Stopped: ${name}`);
+          return await interaction.editReply({
+            embeds: [embed],
+            ephemeral: true,
+          });
+        } else {
+          console.log(response.status, response.data);
+          embed.setDescription(`Something went wrong while stopping: ${name}`);
+          return await interaction.editReply({
+            embeds: [embed],
+            ephemeral: true,
+          });
+        }
+      }
     } catch (error) {
       await interaction.reply({
         content: `Error: ${error}`,
