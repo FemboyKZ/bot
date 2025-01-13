634187273d0d0485fbeea3d9c5834e0ae194ab77
Dots <dotvnl@gmail.com>
2025-01-13 20:38:46 +0200

    remove stupid shit

--- src/reaction-roles.js
+++ src/reaction-roles.js
@@ -2,12 +2,6 @@ const { Events } = require("discord.js");
 const schema = require("./Schemas/reactionrole.js");
 const { client } = require("./index.js");
 
-let lastOnlineTime = Date.now();
-
-client.on("ready", () => {
-  lastOnlineTime = Date.now();
-});
-
 client.on(Events.MessageReactionAdd, async (reaction, user) => {
   if (!reaction || !reaction.message || !reaction.message.guildId || user.bot) {
     return;
@@ -17,39 +11,27 @@ client.on(Events.MessageReactionAdd, async (reaction, user) => {
   if (!reaction.emoji.id) {
     cID = reaction.emoji.name;
   }
+  const data = await schema.findOne({
+    Guild: reaction.message.guildId,
+    Message: reaction.message.id,
+    Emoji: cID,
+  });
+  if (!data) {
+    return;
+  }
+
+  const guild = await client.guilds.cache.get(reaction.message.guildId);
+  if (!guild) {
+    return;
+  }
+
+  const member = await guild.members.cache.get(user.id);
+  if (!member) {
+    return;
+  }
 
   try {
-    const data = await schema.findOne({
-      Guild: reaction.message.guildId,
-      Message: reaction.message.id,
-      Emoji: cID,
-    });
-
-    if (!data) {
-      return;
-    }
-
-    const guild = await client.guilds.cache.get(reaction.message.guildId);
-    if (!guild) {
-      return;
-    }
-
-    const member = await guild.members.cache.get(user.id);
-    if (!member) {
-      return;
-    }
-
     await member.roles.add(data.Role);
-
-    const now = Date.now();
-    const offlineTime = lastOnlineTime;
-    const reactionTime = reaction.createdTimestamp;
-    const offlineDuration = now - offlineTime;
-    const reactionDuration = reactionTime - offlineTime;
-
-    if (reactionDuration > offlineDuration) {
-      await member.roles.add(data.Role);
-    }
   } catch (e) {
     console.error(e);
   }
@@ -65,38 +47,27 @@ client.on(Events.MessageReactionRemove, async (reaction, user) => {
     cID = reaction.emoji.name;
   }
 
+  const data = await schema.findOne({
+    Guild: reaction.message.guildId,
+    Message: reaction.message.id,
+    Emoji: cID,
+  });
+  if (!data) {
+    return;
+  }
+
+  const guild = await client.guilds.cache.get(reaction.message.guildId);
+  if (!guild) {
+    return;
+  }
+
+  const member = await guild.members.cache.get(user.id);
+  if (!member) {
+    return;
+  }
+
   try {
-    const data = await schema.findOne({
-      Guild: reaction.message.guildId,
-      Message: reaction.message.id,
-      Emoji: cID,
-    });
-
-    if (!data) {
-      return;
-    }
-
-    const guild = await client.guilds.cache.get(reaction.message.guildId);
-    if (!guild) {
-      return;
-    }
-
-    const member = await guild.members.cache.get(user.id);
-    if (!member) {
-      return;
-    }
-
     await member.roles.remove(data.Role);
-
-    const now = Date.now();
-    const offlineTime = lastOnlineTime;
-    const reactionTime = reaction.createdTimestamp;
-    const offlineDuration = now - offlineTime;
-    const reactionDuration = reactionTime - offlineTime;
-
-    if (reactionDuration > offlineDuration) {
-      await member.roles.remove(data.Role);
-    }
   } catch (e) {
     console.error(e);
   }
