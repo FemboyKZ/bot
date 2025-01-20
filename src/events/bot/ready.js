b0e387914e3199e983c98becd17022739a1dac0c
Dots <dotvnl@gmail.com>
2025-01-20 17:11:29 +0200

    revert ready changes

--- src/events/bot/ready.js
+++ src/events/bot/ready.js
@@ -6,24 +6,34 @@ const mongodbURI = process.env.MONGODB_URI;
 mongoose.set("strictQuery", false);
 mongoose.set("debug", true);
 
-module.exports = (client) => {
-  client.on("ready", async () => {
+module.exports = {
+  name: Events.ClientReady,
+  async execute(client) {
     console.log("Ready!");
 
     if (!mongodbURI) return console.log("Setup mongodbURI in .env");
 
     try {
-      await mongoose.connect(mongodbURI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        //serverSelectionTimeoutMS: 30000,
-      });
+      console.log("Checking connection to MongoDB...");
+      if (mongoose.connection.readyState === 0) {
+        console.log("Not connected, attempting to connect to MongoDB...");
+        await mongoose.connect(mongodbURI, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+          serverSelectionTimeoutMS: 3000,
+          bufferCommands: false,
+        });
+      } else {
+        return console.log(
+          "Already connected/connecting to MongoDB, stuck disconnecting or uninitialized.\nreadyState: ",
+          mongoose.connection.readyState
+        );
+      }
 
       console.log("The MongoDB Database is running.");
     } catch (error) {
       console.error("Error connecting to MongoDB:", error);
     }
-
     await client.guilds.cache.forEach(async (guild) => {
       const clientMember = await guild.members.cache.get(client.user.id);
       if (!clientMember.permissions.has(PermissionsBitField.Flags.ManageGuild))
@@ -35,16 +45,5 @@ module.exports = (client) => {
         new Collection(firstInvites.map((invite) => [invite.code, invite.uses]))
       );
     });
-  });
-  mongoose.connection.on("connected", () => {
-    console.log("Mongoose connection to DB successful");
-  });
-
-  mongoose.connection.on("error", (err) => {
-    console.error("Mongoose connection error: ", err);
-  });
-
-  mongoose.connection.on("disconnected", () => {
-    console.log("Mongoose disconnected");
-  });
+  },
 };
