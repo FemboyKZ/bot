851d4e3f4011206ee2450226041d675c11ee44e9
Dots <dotvnl@gmail.com>
2025-01-21 19:32:45 +0200

    delete unused schemas

--- src/Schemas/moderation/ghostnum.js
+++
@@ -1,9 +0,0 @@
-const { model, Schema } = require("mongoose");
-
-let numSchema = new Schema({
-  Guild: { type: String },
-  User: { type: String },
-  Number: { type: Number },
-});
-
-module.exports = model("ghostNum", numSchema);
--- src/Schemas/moderation/ghostping.js
+++
@@ -1,7 +0,0 @@
-const { model, Schema } = require("mongoose");
-
-let ghostSchema = new Schema({
-  Guild: { type: String, unique: true, required: true },
-});
-
-module.exports = model("ghost", ghostSchema);
