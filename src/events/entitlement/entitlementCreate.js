c77c690f07d48a59170be018ae42fa384ac26b4b
Dots <dotvnl@gmail.com>
2025-01-21 17:21:24 +0200

    add entitlement listeners

---
+++ src/events/entitlement/entitlementCreate.js
@@ -0,0 +1,9 @@
+const { Events } = require("discord.js");
+
+module.exports = {
+  name: Events.EntitlementCreate,
+  async execute(entitlement, client) {
+    // TODO: Implement
+    // wtf does this do?
+  },
+};
---
+++ src/events/entitlement/entitlementDelete.js
@@ -0,0 +1,9 @@
+const { Events } = require("discord.js");
+
+module.exports = {
+  name: Events.EntitlementDelete,
+  async execute(entitlement, client) {
+    // TODO: Implement
+    // wtf does this do?
+  },
+};
