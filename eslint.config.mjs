0f36675caadf31cd3ca876aabdd903e6016fa841
Dots <dotvnl@gmail.com>
2025-01-28 18:57:01 +0200

    add basic cfg for prettier, add eslint plugin for prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,11 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
+  eslintPluginPrettierRecommended,
 ];
