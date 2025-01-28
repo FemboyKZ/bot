3ac4ffdeef311a8d7c45e2b5423b8d605a6226c1
Dots <dotvnl@gmail.com>
2025-01-28 19:08:05 +0200

    ignore unused vars

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,12 @@ import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { languageOptions: { globals: globals.browser } },
+  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   eslintPluginPrettierRecommended,
+  {
+    rules: {
+      "no-unused-vars": "ignore",
+    },
+  },
 ];
