56380e0a5ffd91ee23f55678ebd3e8e1f0b3b44d
Dots <dotvnl@gmail.com>
2025-04-09 20:00:49 +0300

    log whole warning

--- src/events/process/warning.js
+++ src/events/process/warning.js
@@ -1,13 +1,6 @@
 module.exports = {
   name: "warning",
   async execute(warning, client) {
-    console.warn(
-      "Warning: ",
-      warning.name || "Unknown",
-      "Message: ",
-      warning.message || "Unknown",
-      "\nStack: ",
-      warning.stack || "None",
-    );
+    console.warn("Warning: ", warning);
   },
 };
