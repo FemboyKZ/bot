98d796b2fc13f44ca432c7bd735219a03b0d07b7
Dots <dotvnl@gmail.com>
2025-04-09 19:40:32 +0300

    remove stupid shit

--- src/events/process/unhandledRejection.js
+++ src/events/process/unhandledRejection.js
@@ -1,21 +1,8 @@
 module.exports = {
   name: "unhandledRejection",
   async execute(reason, promise, client) {
-    let reasonDetails;
-    if (reason && reason.message) {
-      reasonDetails = reason.message;
-    } else if (reason && reason.stack) {
-      reasonDetails = reason.stack;
-    } else {
-      try {
-        reasonDetails = JSON.stringify(reason);
-      } catch (error) {
-        reasonDetails = "Unknown reason (failed to stringify)";
-      }
-    }
-
     console.error(
-      `Unhandled Rejection!\nPromise: ${promise}\nReason: ${reasonDetails}`,
+      `Unhandled Rejection! - Promise: ${promise} - Reason: ${reason}`,
     );
   },
 };
